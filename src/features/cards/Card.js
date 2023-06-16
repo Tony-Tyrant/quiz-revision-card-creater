1086832a81ee37fbc1558ef609d0eb78329dd279
Tony-Tyrant <tony.tyrant@gmail.com>
2023-06-16 15:36:26 +0800

    fixed bugs for non-used values

--- src/features/cards/Card.js
+++ src/features/cards/Card.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link, useParams } from "react-router-dom";
 import { selectcards } from "./cardsSlice";
 import { useSelector } from "react-redux";
 
